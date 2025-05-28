b2961779fc8115ae123ead96556247620aac418a
Lynrbe <khanhlinh26004@gmail.com>
2025-05-28 23:20:39 +0700

    update ermegency call UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,38 @@
 import 'package:blind_sunglasses/homescreen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:blind_sunglasses/emergencycall.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
+
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+FlutterLocalNotificationsPlugin();
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  var initializationSettingsAndroid =
+      AndroidInitializationSettings('@mipmap/ic_launcher');
+  var initializationSettingsIOS = DarwinInitializationSettings(
+    requestAlertPermission: false,
+    requestBadgePermission: false,
+    requestSoundPermission: false,
+    );
+  var initializationSettings = InitializationSettings(
+    android: initializationSettingsAndroid,
+    iOS: initializationSettingsIOS,
+  );
+  await flutterLocalNotificationsPlugin.initialize(
+      initializationSettings,
+      onDidReceiveNotificationResponse: (NotificationResponse response) async {
+        if (response.payload != null) {
+        // Handle notification response
+          print("Notification payload: ${response.payload}");
+        } else {
+          print("Notification payload is null");
+        }
+      },
+  );
+
   await Firebase.initializeApp();
   runApp(MyApp());
 }
